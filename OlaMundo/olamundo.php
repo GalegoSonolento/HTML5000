93dde265ef869af507d58ebd22d2b1d04fe85ce5
HaagHenrique <henriquehaag11@gmail.com>
2023-05-03 15:31:42 -0300

    primeiro código em php

---
+++ OlaMundo/olamundo.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8"/>
+        <title>Exemplo PHP</title>
+        <style>
+            h2 {
+                color: green;
+                text-shadow: 1px 1px 1px blue;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>Testando PHP</h1>
+        <?php
+        
+            echo "<h2>Olá, <br>Mundo!</br></h2>";
+
+        ?>
+    </body>
+</html>
